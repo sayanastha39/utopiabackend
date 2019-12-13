cf1657f5f4490c7f81fb08a1f998236c2d01dea0
sayanastha39 <54332268+sayanastha39@users.noreply.github.com>
2019-12-13 18:16:23 -0500

    Update LoginController.java

--- src/main/java/com/ss/utopia/utopiaAirline/Controller/LoginController.java
+++ src/main/java/com/ss/utopia/utopiaAirline/Controller/LoginController.java
@@ -2,6 +2,7 @@ package com.ss.utopia.utopiaAirline.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
