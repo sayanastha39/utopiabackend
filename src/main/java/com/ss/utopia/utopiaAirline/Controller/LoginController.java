8ba3941bdf06bd17619251913b325a674791beed
sayanastha39 <54332268+sayanastha39@users.noreply.github.com>
2019-12-19 13:50:05 -0500

    Update LoginController.java
    CORS changes

--- src/main/java/com/ss/utopia/utopiaAirline/Controller/LoginController.java
+++ src/main/java/com/ss/utopia/utopiaAirline/Controller/LoginController.java
@@ -31,7 +31,7 @@ public class LoginController {
 	}
 	
 	 @PostMapping("/users")
-	 @CrossOrigin(origins = "http://utopia-airlines.s3-website.us-east-2.amazonaws.com/#/")
+	 @CrossOrigin(origins = "http://localhost:3000/#/")
 	 @ResponseStatus(code = HttpStatus.OK)
 		public ResponseEntity<String> createUser(@RequestBody User user) {
 		 loginService.createUser(user);
@@ -39,7 +39,7 @@ public class LoginController {
 		}
 	 
 	 @GetMapping ("/users") 
-	 @CrossOrigin(origins = "http://utopia-airlines.s3-website.us-east-2.amazonaws.com/#/")
+	 @CrossOrigin(origins = "http://localhost:3000/#/")
 	 @ResponseStatus(code = HttpStatus.OK)
 		public ResponseEntity<?> readUser(@RequestHeader ("username") String username) {
 		 	
